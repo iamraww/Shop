aef151e735f73a0e3245985f1d796e4c2458def1
Raw <tranduc@gmail.com>
2021-01-17 13:12:01 +0700

    ok

--- routes/product-admin-route.js
+++ routes/product-admin-route.js
@@ -10,8 +10,9 @@ route.get('/', productController.getList);
 route.get('/all-products', productController.allProduct);
 
 // trả về form để nhập thông tin
-route.get('/add-product', productController.create);
-// nhận thông tin từ form, save vào database.
+// route.get('/add-product', productController.create);
+
+// nhận t"hông tin từ form, save vào database.
 route.post('/add-product', productController.save);
 
 route.get('/edit/:id', productController.edit);
