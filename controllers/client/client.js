ba80eeab8f03769aa057e8e8d9d11f1b37b2563c
Raw <tranduc@gmail.com>
2021-01-25 22:47:29 +0700

    ok

--- controllers/client/client.js
+++ controllers/client/client.js
@@ -11,6 +11,12 @@ exports.quickView = async function (req, resp) {
     const lienQuan = await Product.find();
     resp.render('client/product', {obj: obj, other: lienQuan});
 }
+
+exports.loginClient = async function (req, resp) {
+    const obj = await Product.findById(req.params.id);
+    const lienQuan = await Product.find();
+    resp.render('client/login-client', {data: obj});
+}
 // exports.rubyProduct = async function (req, resp) {
 //     const product = await Product.findById(req.params.id);
 //     const lienQuan = await Product.find();
--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -3,6 +3,7 @@ const route = express.Router();
 const clientIndex = require('../controllers/client/client');
 
 route.get('/ruby', clientIndex.rubyProduct);
+route.get('/login', clientIndex.loginClient);
 
 route.get('/product/:id', clientIndex.quickView);
 module.exports = route;
